71996f4893f70f6747fbcdce893c8e56e99f459b
Thomas Teisberg <tteisberg@gmail.com>
2023-04-28 16:08:46 -0700

    Cleanup and plot tweaks

--- plot_helpers.jl
+++ plot_helpers.jl
@@ -6,9 +6,17 @@ function plot_fields(xs, zs, to_plot::OrderedDict)
     fig = Figure(resolution=(1000, 300*length(to_plot)))
 
     for (idx, titles) in enumerate(keys(to_plot))
-        ax = Axis(fig[idx, 1], title=titles[1])
-        h = heatmap!(ax, xs, zs, to_plot[titles])
-        cb = Colorbar(fig[idx, 2], h, label=titles[2])
+        title, axis_label = titles
+
+        ax = Axis(fig[idx, 1], title=title)
+
+        if typeof(to_plot[titles]) <: Tuple
+            h = heatmap!(ax, xs, zs, to_plot[titles][1]; to_plot[titles][2]...)
+            cb = Colorbar(fig[idx, 2], h, label=axis_label)
+        else
+            h = heatmap!(ax, xs, zs, to_plot[titles])
+            cb = Colorbar(fig[idx, 2], h, label=axis_label)
+        end
     end
 
     fig
